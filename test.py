ebfb9b1ec1e5e990b056a1800706bfe994651061
Gabor Berta <Gabor.Berta@quest.com>
2023-05-17 22:51:57 +0200

    tests.py: created tests for allocate_3_bad function

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import unittest
 from allocation import Building
 from allocation import allocate_1, allocate_2, allocate_3, allocate_4
-from allocation import allocate_1_bad, allocate_2_bad
+from allocation import allocate_1_bad, allocate_2_bad, allocate_3_bad
 
 class TestAllocations(unittest.TestCase):
     
-    # Expected bad result for some of the *_bad functions - this is the goal!
+    # Expected bad result for some of the *bad functions - this is the goal to fail some bad tests!
 
     def test_less_than_10_engineering_and_art_science(self):
         self.assertEqual(allocate_1(8, 2), (Building.A, Building.B))
@@ -13,6 +13,7 @@ class TestAllocations(unittest.TestCase):
         self.assertEqual(allocate_2(8, 2), (Building.A, Building.B))
         self.assertEqual(allocate_2_bad(8, 2), (Building.A, Building.B))
         self.assertEqual(allocate_3(8, 2), (Building.A, Building.B))
+        self.assertEqual(allocate_3_bad(8, 2), (Building.A, Building.B))
         self.assertEqual(allocate_4(8, 2), (Building.A, Building.B))
         
     def test_between_10_and_50_engineering_and_art_science(self):
@@ -21,6 +22,7 @@ class TestAllocations(unittest.TestCase):
         self.assertEqual(allocate_2(20, 35), (Building.B, Building.C))
         self.assertEqual(allocate_2_bad(20, 35), (Building.B, Building.C))
         self.assertEqual(allocate_3(20, 35), (Building.B, Building.C))
+        self.assertEqual(allocate_3_bad(20, 35), (Building.B, Building.C))
         self.assertEqual(allocate_4(20, 35), (Building.B, Building.C))
 
     def test_more_than_50_engineering_and_art_science(self):
@@ -29,22 +31,25 @@ class TestAllocations(unittest.TestCase):
         self.assertEqual(allocate_2(60, 75), (Building.D, Building.D))
         self.assertEqual(allocate_2_bad(60, 75), (Building.D, Building.D))
         self.assertEqual(allocate_3(60, 75), (Building.D, Building.D))
+        self.assertEqual(allocate_3_bad(60, 75), (Building.D, Building.D))
         self.assertEqual(allocate_4(60, 75), (Building.D, Building.D))
 
     def test_10_engineering_and_10_art_science(self):
         self.assertEqual(allocate_1(10, 10), (Building.B, Building.C))
         self.assertEqual(allocate_1_bad(10, 10), (Building.B, Building.C))
         self.assertEqual(allocate_2(10, 10), (Building.B, Building.C))
-        self.assertEqual(allocate_2_bad(10, 10), (Building.B, Building.C))
+        self.assertEqual(allocate_2_bad(10, 10), (Building.B, Building.C)) # fails
         self.assertEqual(allocate_3(10, 10), (Building.B, Building.C))
+        self.assertEqual(allocate_3_bad(10, 10), (Building.B, Building.C))
         self.assertEqual(allocate_4(10, 10), (Building.B, Building.C))
 
     def test_50_engineering_and_50_art_science(self):
         self.assertEqual(allocate_1(50, 50), (Building.B, Building.C))
-        self.assertEqual(allocate_1_bad(50, 50), (Building.B, Building.C))
+        self.assertEqual(allocate_1_bad(50, 50), (Building.B, Building.C)) # fails
         self.assertEqual(allocate_2(50, 50), (Building.B, Building.C))
         self.assertEqual(allocate_2_bad(50, 50), (Building.B, Building.C))
         self.assertEqual(allocate_3(50, 50), (Building.B, Building.C))
+        self.assertEqual(allocate_3_bad(50, 50), (Building.B, Building.C)) # fails
         self.assertEqual(allocate_4(50, 50), (Building.B, Building.C))
         
     def test_less_than_10_engineering_and_more_than_50_art_science(self):
@@ -53,6 +58,7 @@ class TestAllocations(unittest.TestCase):
         self.assertEqual(allocate_2(9, 51), (Building.A, Building.D))
         self.assertEqual(allocate_2_bad(9, 51), (Building.A, Building.D))
         self.assertEqual(allocate_3(9, 51), (Building.A, Building.D))
+        self.assertEqual(allocate_3_bad(9, 51), (Building.A, Building.D))
         self.assertEqual(allocate_4(9, 51), (Building.A, Building.D))
 
 
