da82399516e2785508e7c16fe13c586b32622361
Gabor Berta <Gabor.Berta@quest.com>
2023-05-17 21:56:56 +0200

    test.py: added test for allocate_2_function

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
 import unittest
-from allocation import allocate_1, Building
+from allocation import Building
+from allocation import allocate_1, allocate_2
 
 class TestAllocations(unittest.TestCase):
     
     def test_less_than_10_engineering_and_art_science(self):
         self.assertEqual(allocate_1(8, 2), (Building.A, Building.B))
+        self.assertEqual(allocate_2(8, 2), (Building.A, Building.B))
         
     def test_between_10_and_50_engineering_and_art_science(self):
         self.assertEqual(allocate_1(20, 35), (Building.B, Building.C))
+        self.assertEqual(allocate_2(20, 35), (Building.B, Building.C))
         
     def test_more_than_50_engineering_and_art_science(self):
         self.assertEqual(allocate_1(60, 75), (Building.D, Building.D))
+        self.assertEqual(allocate_2(60, 75), (Building.D, Building.D))
         
     def test_less_than_10_engineering_and_more_than_50_art_science(self):
         self.assertEqual(allocate_1(9, 51), (Building.A, Building.D))
+        self.assertEqual(allocate_2(9, 51), (Building.A, Building.D))
 
 if __name__ == '__main__':
     unittest.main()
