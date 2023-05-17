0c5f022a690947e99fd279563775eec0dcdb5544
Gabor Berta <Gabor.Berta@quest.com>
2023-05-17 22:17:02 +0200

    tests.py: test the edge cases (numbers)

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 from allocation import Building
 from allocation import allocate_1, allocate_2, allocate_3, allocate_4
+from allocation import allocate_1_bad
 
 class TestAllocations(unittest.TestCase):
     
@@ -21,12 +22,25 @@ class TestAllocations(unittest.TestCase):
         self.assertEqual(allocate_2(60, 75), (Building.D, Building.D))
         self.assertEqual(allocate_3(60, 75), (Building.D, Building.D))
         self.assertEqual(allocate_4(60, 75), (Building.D, Building.D))
+
+    def test_10_engineering_and_10_art_science(self):
+        self.assertEqual(allocate_1(10, 10), (Building.B, Building.C))
+        self.assertEqual(allocate_2(10, 10), (Building.B, Building.C))
+        self.assertEqual(allocate_3(10, 10), (Building.B, Building.C))
+        self.assertEqual(allocate_4(10, 10), (Building.B, Building.C))
+
+    def test_50_engineering_and_50_art_science(self):
+        self.assertEqual(allocate_1(50, 50), (Building.B, Building.C))
+        self.assertEqual(allocate_2(50, 50), (Building.B, Building.C))
+        self.assertEqual(allocate_3(50, 50), (Building.B, Building.C))
+        self.assertEqual(allocate_4(50, 50), (Building.B, Building.C))
         
     def test_less_than_10_engineering_and_more_than_50_art_science(self):
         self.assertEqual(allocate_1(9, 51), (Building.A, Building.D))
         self.assertEqual(allocate_2(9, 51), (Building.A, Building.D))
         self.assertEqual(allocate_3(9, 51), (Building.A, Building.D))
         self.assertEqual(allocate_4(9, 51), (Building.A, Building.D))
+
 
 if __name__ == '__main__':
     unittest.main()
