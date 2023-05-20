0616c6c9e26340b78880d156549014ee4fa2102f
Gabor Berta <Gabor.Berta@quest.com>
2023-05-20 22:58:46 +0200

    allocation.py: Handle if course students sum over 50 case

--- allocation.py
+++ allocation.py
@@ -28,22 +28,28 @@ def get_art_and_science_students_number(default_value):
     else:
         return default_value
 
+def is_sum_over_50(engineering_students, art_science_students):
+    return engineering_students + art_science_students > 50
+
+def get_bigger_number(engineering_students, art_science_students):
+    # in case of equality return art_science_students, because of the requirement
+    if engineering_students > art_science_students:
+        return "engineers"
+    else:
+        return "art_science"
 
 def allocate_1(engineering_students, art_science_students):
     engineer_course = Building.Default
     art_science_course = Building.Default
-    selected_engineer = False
-    selected_art_science = False
 
-    if engineering_students + art_science_students > 50:
-        if engineering_students > art_science_students:
+    if is_sum_over_50(engineering_students, art_science_students):
+        bigger_group = get_bigger_number(engineering_students, art_science_students)
+        if bigger_group == "engineers":
             engineer_course = Building.D
-            selected_engineer = True
-        if engineering_students < art_science_students:
+        if bigger_group == "art_science":
             art_science_course = Building.D
-            selected_art_science = True
 
-    if selected_engineer == False:
+    if engineer_course != Building.D:
         if engineering_students < 10:
             engineer_course = Building.A
         elif 10 <= engineering_students <= 50:
@@ -51,7 +57,7 @@ def allocate_1(engineering_students, art_science_students):
         elif engineering_students > 50:
             engineer_course = Building.D
 
-    if selected_art_science == False:
+    if art_science_course != Building.D:
         if art_science_students < 10:
             art_science_course = Building.B
         elif 10 <= art_science_students <= 50:
@@ -66,106 +72,160 @@ def allocate_1_bad(engineering_students, art_science_students):
     engineer_course = Building.Default
     art_science_course = Building.Default
 
-    if engineering_students < 10:
-        engineer_course = Building.A
-    if 10 <= engineering_students <= 50:
-        engineer_course = Building.B
-    if engineering_students >= 50:
-        engineer_course = Building.D
-    
-    if art_science_students < 10:
-        art_science_course = Building.B
-    if 10 <= art_science_students <= 50:
-        art_science_course = Building.C
-    if art_science_students >= 50:
-        art_science_course = Building.D
+    if is_sum_over_50(engineering_students, art_science_students):
+        bigger_group = get_bigger_number(engineering_students, art_science_students)
+        if bigger_group == "engineers":
+            engineer_course = Building.D
+        if bigger_group == "art_science":
+            art_science_course = Building.D
+
+    if engineer_course == Building.D:
+        if engineering_students < 10:
+            engineer_course = Building.A
+        elif 10 <= engineering_students <= 50:
+            engineer_course = Building.B
+        elif engineering_students > 50:
+            engineer_course = Building.D
+
+    if art_science_course != Building.D:
+        if art_science_students < 10:
+            art_science_course = Building.B
+        elif 10 <= art_science_students <= 50:
+            art_science_course = Building.C
+        elif art_science_students > 50:
+            art_science_course = Building.D
     
     return (engineer_course, art_science_course)
 
 
 def allocate_2(engineering_students, art_science_students):
-    engineering_students_map = {
-        engineering_students < 10: Building.A,
-        10 <= engineering_students <= 50: Building.B,
-        engineering_students > 50: Building.D,
-    }
-
-    engineer_course = Building.Default
-    for condition, building in engineering_students_map.items():
-        if condition:
-            engineer_course = building
-            break
-
-    art_science_students_map = {
-        art_science_students < 10: Building.B,
-        10 <= art_science_students <= 50: Building.C,
-        art_science_students > 50: Building.D,
-    }
-
+    engineer_course = Building.Default   
     art_science_course = Building.Default
-    for condition, building in art_science_students_map.items():
-        if condition:
-            art_science_course = building
-            break
+
+    if is_sum_over_50(engineering_students, art_science_students):
+        bigger_group = get_bigger_number(engineering_students, art_science_students)
+        if bigger_group == "engineers":
+            engineer_course = Building.D
+        if bigger_group == "art_science":
+            art_science_course = Building.D
+
+    if engineer_course != Building.D:
+        engineering_students_map = {
+            engineering_students < 10: Building.A,
+            10 <= engineering_students <= 50: Building.B,
+            engineering_students > 50: Building.D,
+        }
+    
+        for condition, building in engineering_students_map.items():
+            if condition:
+                engineer_course = building
+                break
+
+    if art_science_course != Building.D:
+        art_science_students_map = {
+            art_science_students < 10: Building.B,
+            10 <= art_science_students <= 50: Building.C,
+            art_science_students > 50: Building.D,
+        }
+
+        for condition, building in art_science_students_map.items():
+            if condition:
+                art_science_course = building
+                break
 
     return (engineer_course, art_science_course)
 
 
 def allocate_2_bad(engineering_students, art_science_students):
-    engineering_students_map = {
-        engineering_students < 10: Building.A,
-        10 < engineering_students <= 50: Building.B,
-        engineering_students > 50: Building.D,
-    }
-
-    engineer_course = Building.Default
-    for condition, building in engineering_students_map.items():
-        if condition:
-            engineer_course = building
-
-    art_science_students_map = {
-        art_science_students < 10: Building.B,
-        10 < art_science_students <= 50: Building.C,
-        art_science_students > 50: Building.D,
-    }
-
+    engineer_course = Building.Default   
     art_science_course = Building.Default
-    for condition, building in art_science_students_map.items():
-        if condition:
-            art_science_course = building
+
+    if is_sum_over_50(engineering_students, art_science_students):
+        bigger_group = get_bigger_number(art_science_students, engineering_students)
+        if bigger_group == "engineers":
+            engineer_course = Building.D
+        if bigger_group == "art_science":
+            art_science_course = Building.D
+
+    if engineer_course != Building.D:
+        engineering_students_map = {
+            engineering_students < 10: Building.A,
+            10 <= engineering_students <= 50: Building.B,
+            engineering_students > 50: Building.D,
+        }
+    
+        for condition, building in engineering_students_map.items():
+            if condition:
+                engineer_course = building
+                break
+
+    if art_science_course != Building.D:
+        art_science_students_map = {
+            art_science_students < 10: Building.B,
+            10 <= art_science_students <= 50: Building.C,
+            art_science_students > 50: Building.D,
+        }
+
+        for condition, building in art_science_students_map.items():
+            if condition:
+                art_science_course = building
+                break
 
     return (engineer_course, art_science_course)
 
 
 def allocate_3(engineering_students, art_science_students):
-    engineer_course = (
-        Building.A if engineering_students < 10
-        else Building.B if 10 <= engineering_students <= 50
-        else Building.D
-    )
+    engineer_course = Building.Default   
+    art_science_course = Building.Default
 
-    art_science_course = (
-        Building.B if art_science_students < 10
-        else Building.C if 10 <= art_science_students <= 50
-        else Building.D
-    )
+    if is_sum_over_50(engineering_students, art_science_students):
+        bigger_group = get_bigger_number(engineering_students, art_science_students)
+        if bigger_group == "engineers":
+            engineer_course = Building.D
+        if bigger_group == "art_science":
+            art_science_course = Building.D
+
+    if engineer_course != Building.D:
+        engineer_course = (
+            Building.A if engineering_students < 10
+            else Building.B if 10 <= engineering_students <= 50
+            else Building.D
+        )
+
+    if art_science_course != Building.D:
+        art_science_course = (
+            Building.B if art_science_students < 10
+            else Building.C if 10 <= art_science_students <= 50
+            else Building.D
+        )
 
     return (engineer_course, art_science_course)
 
 
 def allocate_3_bad(engineering_students, art_science_students):
-    engineer_course = (
-        Building.A if engineering_students < 10
-        else Building.B if 10 <= engineering_students < 50
-        else Building.D
-    )
+    engineer_course = Building.Default   
+    art_science_course = Building.Default
 
-    art_science_course = (
-        Building.B if art_science_students < 10
-        else Building.C if 10 <= art_science_students < 50
-        else Building.D
-    )
+    if is_sum_over_50(engineering_students, art_science_students):
+        bigger_group = get_bigger_number(engineering_students, art_science_students)
+        if bigger_group == "engineers":
+            engineer_course = Building.D
+        if bigger_group == "art_science":
+            art_science_course = Building.D
 
+    if engineer_course != Building.D:
+        engineer_course = (
+            Building.A if engineering_students < 10
+            else Building.B if 10 < engineering_students <= 50
+            else Building.D
+        )
+
+    if art_science_course != Building.D:
+        art_science_course = (
+            Building.B if art_science_students < 10
+            else Building.C if 10 < art_science_students <= 50
+            else Building.D
+        )
     return (engineer_course, art_science_course)
 
 
@@ -176,17 +236,28 @@ def allocate_4(engineering_students, art_science_students):
                 return building
         return Building.Default
 
-    engineer_course = get_building([
-        (engineering_students < 10, Building.A),
-        (10 <= engineering_students <= 50, Building.B),
-        (engineering_students > 50, Building.D),
-    ])
+    engineer_course = Building.Default   
+    art_science_course = Building.Default
+    if is_sum_over_50(engineering_students, art_science_students):
+        bigger_group = get_bigger_number(engineering_students, art_science_students)
+        if bigger_group == "engineers":
+            engineer_course = Building.D
+        if bigger_group == "art_science":
+            art_science_course = Building.D
 
-    art_science_course = get_building([
-        (art_science_students < 10, Building.B),
-        (10 <= art_science_students <= 50, Building.C),
-        (art_science_students > 50, Building.D),
-    ])
+    if engineer_course != Building.D:
+        engineer_course = get_building([
+            (engineering_students < 10, Building.A),
+            (10 <= engineering_students <= 50, Building.B),
+            (engineering_students > 50, Building.D),
+        ])
+
+    if art_science_course != Building.D:
+        art_science_course = get_building([
+            (art_science_students < 10, Building.B),
+            (10 <= art_science_students <= 50, Building.C),
+            (art_science_students > 50, Building.D),
+        ])
 
     return (engineer_course, art_science_course)
 
@@ -198,17 +269,28 @@ def allocate_4_bad(engineering_students, art_science_students):
                 return building
         return Building.Default
 
-    engineer_course = get_building([
-        (engineering_students < 10, Building.A),
-        (10 >= engineering_students <= 50, Building.B),
-        (engineering_students > 50, Building.D),
-    ])
+    engineer_course = Building.Default   
+    art_science_course = Building.Default
+    if is_sum_over_50(engineering_students, art_science_students):
+        bigger_group = get_bigger_number(engineering_students, art_science_students)
+        if bigger_group == "engineers":
+            engineer_course = Building.D
+        if bigger_group == "art_science":
+            art_science_course = Building.D
 
-    art_science_course = get_building([
-        (art_science_students < 10, Building.B),
-        (10 >= art_science_students <= 50, Building.C),
-        (art_science_students > 50, Building.D),
-    ])
+    if engineer_course != Building.D:
+        engineer_course = get_building([
+            (engineering_students < 10, Building.A),
+            (10 <= engineering_students <= 50, Building.B),
+            (engineering_students > 50, Building.D),
+        ])
+
+    if engineer_course != Building.D:
+        art_science_course = get_building([
+            (art_science_students < 10, Building.B),
+            (10 <= art_science_students <= 50, Building.C),
+            (art_science_students > 50, Building.D),
+        ])
 
     return (engineer_course, art_science_course)
 
@@ -226,5 +308,9 @@ if ART_AND_SCIENCE_STUDENTS < 0:
 
 print("ENGINEERING_STUDENTS: ", ENGINEERING_STUDENTS)
 print("ART_AND_SCIENCE_STUDENTS: ", ART_AND_SCIENCE_STUDENTS)
-print(allocate_1(ENGINEERING_STUDENTS, ART_AND_SCIENCE_STUDENTS))
+
+print("allocate_1: ",allocate_1(ENGINEERING_STUDENTS, ART_AND_SCIENCE_STUDENTS))
+print("allocate_2: ",allocate_2(ENGINEERING_STUDENTS, ART_AND_SCIENCE_STUDENTS))
+print("allocate_3: ",allocate_3(ENGINEERING_STUDENTS, ART_AND_SCIENCE_STUDENTS))
+print("allocate_4: ",allocate_4(ENGINEERING_STUDENTS, ART_AND_SCIENCE_STUDENTS))
 '''
